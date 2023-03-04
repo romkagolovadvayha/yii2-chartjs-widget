74126ba2d3da9c6d66694381defd01a5a5b2652a
Roman Mescheryakov <rom_ik@bk.ru>
2023-03-05 01:41:21 +0400

    Update ChartJsAsset.php
    to 4.2.1 version

--- src/ChartJsAsset.php
+++ src/ChartJsAsset.php
@@ -20,7 +20,7 @@ class ChartJsAsset extends AssetBundle
     public $sourcePath = null;
 
     public $js = [
-        'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.1.2/chart.umd.js'
+        'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.1/chart.umd.js'
     ];
 
     public $depends = [
